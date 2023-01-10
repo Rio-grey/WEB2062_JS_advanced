aa0882b3d81e3a64a23c032b553a5f117c69b8ab
Rio-grey <lequy11082003@gmail.com>
2023-01-10 09:53:34 +0700

    rio

--- Data Structures, Modern Operators and Strings/logical assignment operators.js
+++ Data Structures, Modern Operators and Strings/logical assignment operators.js
@@ -44,7 +44,8 @@ const restaurant = {
 
 const rest1 = {
   name: "Capri",
-  numGuests: 20,
+  // numGuests: 20,
+  numGuests: 0,
 };
 
 const rest2 = {
@@ -52,8 +53,23 @@ const rest2 = {
   owner: "Giovanni Rossi",
 };
 
+// OR assignment operator
 rest1.numGuests = rest1.numGuests || 10;
 rest2.numGuests = rest2.numGuests || 10;
+
+// cách 2
+// rest1.numGuests ||= 10;
+// rest2.numGuests ||= 10;
+
+// nullish assignment operator
+rest1.numGuests ??= 10;
+rest2.numGuests ??= 10;
+
+// AND assignment operator
+rest1.owner = rest1.owner && "<ANONYMOUS>";
+rest2.owner = rest2.owner && "<ANONYMOUS>";
+rest1.owner &&= "ANONYMOUS";
+rest2.owner &&= "ANONYMOUS";
 
 console.log(rest1);
 console.log(rest2);
