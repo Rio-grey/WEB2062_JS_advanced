ae8099243df59ad83969c916c1040b7c874a5afd
Rio-grey <lequy11082003@gmail.com>
2023-02-07 17:21:55 +0700

    looping arrays: forEach

---
+++ Working With Arrays/looping arrays (foreach).js
@@ -0,0 +1,20 @@
+const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+
+// for (const movement of movements) {
+for (const [i, movement] of movements.entries()) {
+  if (movement > 0) {
+    console.log(`Movement ${i + 1}: You deposited ${movement}`);
+  } else {
+    console.log(`Movement ${i + 1}: You withdrew ${Math.abs(movement)}`);
+  }
+}
+console.log("---- FOREACH ----");
+// movement là phần tử hiện tại. index là chỉ mục
+// break và continue k có tác dụng trong forEach
+movements.forEach((movement, index, array) => {
+  if (movement > 0) {
+    console.log(`Movement ${index + 1}: You deposited ${movement}`);
+  } else {
+    console.log(`Movement ${index + 1}: You withdrew ${Math.abs(movement)}`);
+  }
+});
