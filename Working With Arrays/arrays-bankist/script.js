7e87403ea40f16f18571c5e79c49941643ef132b
Rio-grey <lequy11082003@gmail.com>
2023-02-12 16:39:32 +0700

    computing usernames

--- Working With Arrays/arrays-bankist/script.js
+++ Working With Arrays/arrays-bankist/script.js
@@ -79,6 +79,18 @@ const displayMovements = function (movements) {
 };
 displayMovements(account1.movements);
 
+const createUsernames = function (accs) {
+  accounts.forEach(function (acc) {
+    acc.username = acc.owner
+      .toLowerCase()
+      .split(" ")
+      .map((name) => name[0])
+      .join("");
+  });
+};
+createUsernames(accounts);
+console.log(accounts);
+
 /////////////////////////////////////////////////
 /////////////////////////////////////////////////
 // LECTURES
